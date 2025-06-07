d6ac6be2df5e75efa6612b1e66acdd0d3e7a5fca
Mdhasim-tech <131744384+Mdhasim-tech@users.noreply.github.com>
2025-06-07 17:08:46 +0530

    Update page.jsx

--- frontend/app/predict/page.jsx
+++ frontend/app/predict/page.jsx
@@ -52,7 +52,7 @@ export default function PredictPage() {
       Property_Area: map[formData.Property_Area],
     }
 
-    const res = await fetch('http://127.0.0.1:5000/predict/', {
+    const res = await fetch('https://loan-approval-app-backend.onrender.com/predict/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload)
